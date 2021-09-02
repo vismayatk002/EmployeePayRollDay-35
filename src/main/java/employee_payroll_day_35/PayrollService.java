1b7afdeb5319c636ed5db765f70b8954a830de22
vismayatk002 <vismayatk002@gmail.com>
2021-09-02 15:48:54 +0530

    Added welcome message

---
+++ src/main/java/employee_payroll_day_35/PayrollService.java
@@ -0,0 +1,9 @@
+package employee_payroll_day_35;
+
+public class PayrollService {
+	
+    public static void main( String[] args ){
+    	
+        System.out.println( "Welcome to Employee Payroll Service" );
+    }
+}
